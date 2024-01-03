87b318f4e0c3de601db3da24acb2ade42fb44fa5
matheusssilva991 <matheus.ssilva991@gmail.com>
2024-01-03 19:59:01 -0300

    feat: att inicial projeto

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <h1>Header</h1>
+        <p>Isso aqui é um header caralho.</p>
+    </header>
+</template>
+
+<style scoped>
+</style>
+
+<script setup>
+</script>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="register">
+    <h1>This is an register page</h1>
+  </div>
+</template>
+
+<style>
+</style>
