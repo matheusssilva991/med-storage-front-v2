d5b5573b25fc1f760e15a865da9dffd4391e292e
matheusssilva991 <matheus.ssilva991@gmail.com>
2024-01-12 01:06:01 -0300

    feat: toast messages

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,10 +1,12 @@
 <template>
     <header class="header">
         <h1>Header</h1>
-        <div>
+        <div class="header-nav">
             <router-link to="/">Home</router-link>
             <router-link to="/register">Register</router-link>
-            <ButtonComp type="btn-primary" text="Button" icon="fa-solid fa-user-secret"/>
+            <ButtonComp type="btn-primary" text="Button" icon="fa-solid fa-user-secret" @click="success"/>
+            <ButtonComp type="btn-secondary" text="Button" icon="fa-solid fa-user-secret" @click="info"/>
+            <ButtonComp type="btn-danger" text="Button" icon="fa-solid fa-user-secret" @click="error"/>
         </div>
     </header>
 </template>
@@ -18,8 +20,31 @@
     background-color: #FFFF;
 }
 
+.header-nav {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: fit-content;
+    width: 100%;
+    margin-top: 1rem;
+    gap: 1rem;
+}
+
 </style>
 
 <script setup lang="ts">
 import ButtonComp from './ButtonComp.vue'
+import { toast } from 'vue3-toastify';
+
+const success = () => {
+  toast.success('Logado com sucesso.');
+}
+
+const info = () => {
+  toast.info('Nada importante');
+}
+
+const error = () => {
+  toast.error('Erro ao logar.');
+}
 </script>
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,18 @@ import router from './router'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
 const app = createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
+.use(
+    Vue3Toasity,
+    {
+        autoClose: 2000,
+        position: 'top-right',
+    } as ToastContainerOptions
+)
 
 app.use(router)
 
